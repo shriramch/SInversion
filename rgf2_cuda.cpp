426316ff6653d802c29a459bbac3e812a37aa252
Yifan Bao <yifbao@student.ethz.ch>
2023-12-16 23:20:17 +0100

    previous testing version of rgf2_cuda not for benchmarking

--- rgf2_cuda.cpp
+++ rgf2_cuda.cpp
@@ -620,107 +620,107 @@ void rgf2sided_lowerprocess_cuda(Matrix &input_A, Matrix &input_G,
     cusolverDnDestroy(cusolverHandle);
 }
 
-typedef struct {
-    int matrixSize;
-    int blockSize;
-    int numRuns;
-    bool isSymmetric;
-    bool saveOffDiag;
-    char *inputPath;
-} Config;
+// typedef struct {
+//     int matrixSize;
+//     int blockSize;
+//     int numRuns;
+//     bool isSymmetric;
+//     bool saveOffDiag;
+//     char *inputPath;
+// } Config;
 
-void InitOptions(Config *config) {
-    config->blockSize = 2;
-    config->matrixSize = 0;
-    config->numRuns = 10;
-    config->isSymmetric = false;
-    config->saveOffDiag = true;
-    config->inputPath = NULL;
-}
+// void InitOptions(Config *config) {
+//     config->blockSize = 2;
+//     config->matrixSize = 0;
+//     config->numRuns = 10;
+//     config->isSymmetric = false;
+//     config->saveOffDiag = true;
+//     config->inputPath = NULL;
+// }
 
-int parse(Config *config, int argc, const char **argv) {
-    static const char *const usages[] = {
-        NULL,
-    };
-    struct argparse_option options[] = {
-        OPT_HELP(),
-        OPT_INTEGER('m', "matrixSize", &config->matrixSize, "matrix size",
-        NULL,
-                    0, 0),
-        OPT_INTEGER('b', "blockSize", &config->blockSize, "block size", NULL,
-        0,
-                    0),
-        OPT_INTEGER('n', "numRuns", &config->numRuns, "number of runs", NULL,
-        0,
-                    0),
-        OPT_INTEGER('s', "isSymmetric", &config->isSymmetric, "is symmetric",
-                    NULL, 0, 0),
-        OPT_INTEGER('o', "saveOffDiag", &config->saveOffDiag, "save off diag", NULL, 0, 0),
-        OPT_STRING('f', "inputPath", &config->inputPath, "input path", NULL,0,0),
-        OPT_END(),
-    };
+// int parse(Config *config, int argc, const char **argv) {
+//     static const char *const usages[] = {
+//         NULL,
+//     };
+//     struct argparse_option options[] = {
+//         OPT_HELP(),
+//         OPT_INTEGER('m', "matrixSize", &config->matrixSize, "matrix size",
+//         NULL,
+//                     0, 0),
+//         OPT_INTEGER('b', "blockSize", &config->blockSize, "block size", NULL,
+//         0,
+//                     0),
+//         OPT_INTEGER('n', "numRuns", &config->numRuns, "number of runs", NULL,
+//         0,
+//                     0),
+//         OPT_INTEGER('s', "isSymmetric", &config->isSymmetric, "is symmetric",
+//                     NULL, 0, 0),
+//         OPT_INTEGER('o', "saveOffDiag", &config->saveOffDiag, "save off diag", NULL, 0, 0),
+//         OPT_STRING('f', "inputPath", &config->inputPath, "input path", NULL,0,0),
+//         OPT_END(),
+//     };
 
-    struct argparse argparse;
-    argparse_init(&argparse, options, usages, 0);
-    argparse_describe(&argparse, "DPHPC TEAM", NULL);
-    argc = argparse_parse(&argparse, argc, argv);
+//     struct argparse argparse;
+//     argparse_init(&argparse, options, usages, 0);
+//     argparse_describe(&argparse, "DPHPC TEAM", NULL);
+//     argc = argparse_parse(&argparse, argc, argv);
 
-    return 0;
-}
+//     return 0;
+// }
 
 // TEMP main to test stuff out
-int main(int argc, const char *argv[]) {
-    const char *bin_name = argv[0];
-    Config config;
-    InitOptions(&config);
-    parse(&config, argc, argv);
+// int main(int argc, const char *argv[]) {
+//     const char *bin_name = argv[0];
+//     Config config;
+//     InitOptions(&config);
+//     parse(&config, argc, argv);
 
-    MPI_Init(&argc, (char ***)(&argv));
-    MPI_Comm_rank(MPI_COMM_WORLD, &processRank);
+//     MPI_Init(&argc, (char ***)(&argv));
+//     MPI_Comm_rank(MPI_COMM_WORLD, &processRank);
 
-    if (config.inputPath != NULL) {
-        // read matrix from file
-    } else if (config.matrixSize != 0) {
-        // generate matrix
-        int MATRIX_SIZE = config.matrixSize;
-        int BLOCK_SIZE = config.blockSize;
-        assert(MATRIX_SIZE % BLOCK_SIZE == 0);
-        int NUM_RUNS = config.numRuns;
-        bool IS_SYMMETRIC = config.isSymmetric;
-        bool SAVE_OFF_DIAG = config.saveOffDiag;
+//     if (config.inputPath != NULL) {
+//         // read matrix from file
+//     } else if (config.matrixSize != 0) {
+//         // generate matrix
+//         int MATRIX_SIZE = config.matrixSize;
+//         int BLOCK_SIZE = config.blockSize;
+//         assert(MATRIX_SIZE % BLOCK_SIZE == 0);
+//         int NUM_RUNS = config.numRuns;
+//         bool IS_SYMMETRIC = config.isSymmetric;
+//         bool SAVE_OFF_DIAG = config.saveOffDiag;
 
-        Matrix inputMatrix =
-            generateBandedDiagonalMatrix(MATRIX_SIZE, 2, true, 0);
-        // Matrix inputMatrix = generateFixedMatrixOfSize4();
-        // inputMatrix.convertDenseToBlkTridiag(BLOCK_SIZE);
+//         Matrix inputMatrix =
+//             generateBandedDiagonalMatrix(MATRIX_SIZE, 2, true, 0);
+//         // Matrix inputMatrix = generateFixedMatrixOfSize4();
+//         // inputMatrix.convertDenseToBlkTridiag(BLOCK_SIZE);
 
-        // if (processRank == 0) {
-        //     inputMatrix.printB();
-        // }
+//         // if (processRank == 0) {
+//         //     inputMatrix.printB();
+//         // }
 
-        Matrix tempResult(
-            MATRIX_SIZE); // zero initialization, same shape as inputMatrix
-        tempResult.convertDenseToBlkTridiag(
-            BLOCK_SIZE); // G has same blockSize as inputMatrix
-        rgf2sided_cuda(inputMatrix, tempResult, IS_SYMMETRIC, SAVE_OFF_DIAG);
+//         Matrix tempResult(
+//             MATRIX_SIZE); // zero initialization, same shape as inputMatrix
+//         tempResult.convertDenseToBlkTridiag(
+//             BLOCK_SIZE); // G has same blockSize as inputMatrix
+//         rgf2sided_cuda(inputMatrix, tempResult, IS_SYMMETRIC, SAVE_OFF_DIAG);
 
-        if (processRank == 0) {
-            std::cout << "\n\nCUDA RESULT\n\n";
-            tempResult.printB();
-        }
+//         if (processRank == 0) {
+//             std::cout << "\n\nCUDA RESULT\n\n";
+//             tempResult.printB();
+//         }
 
-        // Check against the already implemented RGF2 on C++
-        Matrix tempResult_cpp(
-            MATRIX_SIZE); // zero initialization, same shape as inputMatrix
-        tempResult_cpp.convertDenseToBlkTridiag(
-            BLOCK_SIZE); // G has same blockSize as inputMatrix
+//         // Check against the already implemented RGF2 on C++
+//         Matrix tempResult_cpp(
+//             MATRIX_SIZE); // zero initialization, same shape as inputMatrix
+//         tempResult_cpp.convertDenseToBlkTridiag(
+//             BLOCK_SIZE); // G has same blockSize as inputMatrix
 
-        rgf2sided(inputMatrix, tempResult_cpp, false, true);
+//         rgf2sided(inputMatrix, tempResult_cpp, false, true);
 
-        if (processRank == 0) {
-            std::cout << "\n\nC++ RESULT \n\n";
-            tempResult_cpp.printB();
-        }
-    }
-    MPI_Finalize();
-}
+//         if (processRank == 0) {
+//             std::cout << "\n\nC++ RESULT \n\n";
+//             tempResult_cpp.printB();
+//         }
+//     }
+//     MPI_Finalize();
+// }
